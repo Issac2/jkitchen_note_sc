2589a922c50ac1e5196ce0ce5e663060dbac779d
liujinxin <liujinxin@gbase.cn>
2018-07-17 14:39:52 +0800

    by: liujinxin 修改为 stream 方式 收集断路器

--- sc-demo-turbine/src/main/java/com/sauzny/scdemoturbine/TurbineApp.java
+++ sc-demo-turbine/src/main/java/com/sauzny/scdemoturbine/TurbineApp.java
@@ -3,10 +3,12 @@ package com.sauzny.scdemoturbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 
 @SpringBootApplication
-@EnableTurbine // 此注解包含了服务注册客户端注解
+//@EnableTurbine// 此注解包含了服务注册客户端注解
+@EnableTurbineStream // stream方式 使用此注解
 public class TurbineApp {
     public static void main(String[] args) {
         SpringApplication.run(TurbineApp.class);
