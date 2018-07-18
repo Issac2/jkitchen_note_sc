dcdd9ec401196753557184512288ed6988e4ae42
liujinxin <liujinxin@gbase.cn>
2018-07-18 15:13:08 +0800

    by: liujinxin hystrix 加入 rabbitmq

--- sc-demo-turbine/src/main/java/com/sauzny/scdemoturbine/TurbineApp.java
+++ sc-demo-turbine/src/main/java/com/sauzny/scdemoturbine/TurbineApp.java
@@ -2,7 +2,6 @@ package com.sauzny.scdemoturbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 
@@ -10,6 +9,13 @@ import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 //@EnableTurbine// 此注解包含了服务注册客户端注解
 @EnableTurbineStream // stream方式 使用此注解
 public class TurbineApp {
+    
+    // TurbineStream 使用增加
+    //@Bean
+    //public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(CompositeMessageConverterFactory factory) {
+    //    return new ConfigurableCompositeMessageConverter(factory.getMessageConverterForAllRegistered().getConverters());
+    //}
+    
     public static void main(String[] args) {
         SpringApplication.run(TurbineApp.class);
     }
